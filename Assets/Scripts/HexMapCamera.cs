150a937165eea9fd7a98c5d45e923d3b43fa4763
Adrian Ho <54951013+Adrian-HO@users.noreply.github.com>
2024-12-27 16:48:50 -0500

    part 5 2.2 moving

--- Assets/Scripts/HexMapCamera.cs
+++ Assets/Scripts/HexMapCamera.cs
@@ -3,9 +3,41 @@ using UnityEngine;
 public class HexMapCamera : MonoBehaviour {
 
 	Transform swivel, stick;
+    float zoom = 1f;
+    public float stickMinZoom, stickMaxZoom;
+
+    public float swivelMinZoom, swivelMaxZoom;
 
 	void Awake () {
 		swivel = transform.GetChild(0);
 		stick = swivel.GetChild(0);
+	}
+
+    void Update () {
+		float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+		if (zoomDelta != 0f) {
+			AdjustZoom(zoomDelta);
+		}
+
+        float xDelta = Input.GetAxis("Horizontal");
+		float zDelta = Input.GetAxis("Vertical");
+		if (xDelta != 0f || zDelta != 0f) {
+			AdjustPosition(xDelta, zDelta);
+		}
+	}
+
+    void AdjustPosition (float xDelta, float zDelta) {
+        
+	}
+	
+	void AdjustZoom (float delta) 
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+		stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+		swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
 	}
 }
