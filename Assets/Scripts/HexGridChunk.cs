d5d22c81b460acd775829f6518e3031d18cde896
Adrian Ho <adrho9577@yahoo.ca>
2025-01-15 21:16:23 -0500

    4.2 Shore Foam

--- Assets/Scripts/HexGridChunk.cs
+++ Assets/Scripts/HexGridChunk.cs
@@ -5,7 +5,7 @@ public class HexGridChunk : MonoBehaviour
 {
     HexCell[] cells;
 
-    public HexMesh terrain, rivers, roads, water;
+    public HexMesh terrain, rivers, roads, water, waterShore;
     Canvas gridCanvas;
 
     void Awake()
@@ -52,6 +52,7 @@ public class HexGridChunk : MonoBehaviour
         rivers.Clear();
         roads.Clear();
         water.Clear();
+        waterShore.Clear();
         for (int i = 0; i < cells.Length; i++)
         {
             Triangulate(cells[i]);
@@ -60,6 +61,7 @@ public class HexGridChunk : MonoBehaviour
         rivers.Apply();
         roads.Apply();
         water.Apply();
+        waterShore.Apply();
     }
 
     void Triangulate(HexCell cell)
@@ -189,15 +191,27 @@ public class HexGridChunk : MonoBehaviour
 			e1.v1 + bridge,
 			e1.v5 + bridge
 		);
-		water.AddQuad(e1.v1, e1.v2, e2.v1, e2.v2);
-		water.AddQuad(e1.v2, e1.v3, e2.v2, e2.v3);
-		water.AddQuad(e1.v3, e1.v4, e2.v3, e2.v4);
-		water.AddQuad(e1.v4, e1.v5, e2.v4, e2.v5);
+		waterShore.AddQuad(e1.v1, e1.v2, e2.v1, e2.v2);
+		waterShore.AddQuad(e1.v2, e1.v3, e2.v2, e2.v3);
+		waterShore.AddQuad(e1.v3, e1.v4, e2.v3, e2.v4);
+		waterShore.AddQuad(e1.v4, e1.v5, e2.v4, e2.v5);
+        
+        //info about the shore in the V coordinate
+        //0 on the water side and to 1 on the land side
+        waterShore.AddQuadUV(0f, 0f, 0f, 1f);
+		waterShore.AddQuadUV(0f, 0f, 0f, 1f);
+		waterShore.AddQuadUV(0f, 0f, 0f, 1f);
+		waterShore.AddQuadUV(0f, 0f, 0f, 1f);
 
         HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
 		if (nextNeighbor != null) {
-			water.AddTriangle(
+			waterShore.AddTriangle(
 				e1.v5, e2.v5, e1.v5 + HexMetrics.GetBridge(direction.Next())
+			);
+            waterShore.AddTriangleUV(
+				new Vector2(0f, 0f),
+				new Vector2(0f, 1f),
+				new Vector2(0f, nextNeighbor.IsUnderwater ? 0f : 1f)
 			);
 		}
 	}
