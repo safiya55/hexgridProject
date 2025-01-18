b6f982ae162ffd3193e6537de72ead0a6f7cf105
Adrian Ho <adrho9577@yahoo.ca>
2025-01-18 15:56:21 -0500

    1.4 Destroying Features

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 
-public class HexFeatureManager : MonoBehaviour {
+public class HexFeatureManager : MonoBehaviour
+{
 
     public Transform featurePrefab;
 
-	public void Clear () {}
+    public void Clear() { }
 
-	public void Apply () {}
+    public void Apply() { }
 
-	public void AddFeature (Vector3 position) {}
+    public void AddFeature(Vector3 position)
+    {
+        Transform instance = Instantiate(featurePrefab);
+        instance.localPosition = HexMetrics.Perturb(position);;
+    }
 }
