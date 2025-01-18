6346e2de4cfea7bfcc6466f79388c4aaca293587
Adrian Ho <adrho9577@yahoo.ca>
2025-01-18 15:59:07 -0500

    2 Feature Placement

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class HexFeatureManager : MonoBehaviour
 {
+    Transform container;
 
     public Transform featurePrefab;
 
-    public void Clear() { }
+    public void Clear()
+    {
+        //prevent duplicates when chunk refresh. get rid of old feature when chunk is cleared
+        if (container)
+        {
+            Destroy(container.gameObject);
+        }
+        container = new GameObject("Features Container").transform;
+        container.SetParent(transform, false);
+    }
 
     public void Apply() { }
 
     public void AddFeature(Vector3 position)
     {
         Transform instance = Instantiate(featurePrefab);
-        instance.localPosition = HexMetrics.Perturb(position);;
+        instance.localPosition = HexMetrics.Perturb(position); ;
     }
 }
