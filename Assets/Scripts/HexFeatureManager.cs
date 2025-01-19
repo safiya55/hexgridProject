58b8978d08ad236d6887ee29582068074401c481
Adrian Ho <adrho9577@yahoo.ca>
2025-01-19 14:13:21 -0500

    in middle of 5 Multiple Features Prefabs

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -4,7 +4,7 @@ public class HexFeatureManager : MonoBehaviour
 {
     Transform container;
 
-    public Transform featurePrefab;
+    public Transform[] urbanPrefabs;
 
     public void Clear()
     {
@@ -24,15 +24,16 @@ public class HexFeatureManager : MonoBehaviour
         // hash grid to obtain a value. Once we use that to set the rotation, 
         //our features will remain motionless when we edit the terrain.
         HexHash hash = HexMetrics.SampleHashGrid(position);
-        
-        Transform instance = Instantiate(featurePrefab);
-        position.y += instance.localScale.y * 0.5f;
-        instance.localPosition = HexMetrics.Perturb(position);
-        
+
         //bail if 
         if (hash.a >= 0.25f) {
 			return;
 		}
+        
+        Transform instance = Instantiate(urbanPrefabs[cell.UrbanLevel - 1]);
+        position.y += instance.localScale.y * 0.5f;
+        instance.localPosition = HexMetrics.Perturb(position);
+        
 
         //add random rotation to object
         instance.localRotation = Quaternion.Euler(0f, 360f * hash.b, 0f);
