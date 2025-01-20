5071b0aafb72928e299419ccf1b5da65d078f840
Adrian Ho <adrho9577@yahoo.ca>
2025-01-19 20:21:42 -0500

    6 Nultiple Features Types

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -4,7 +4,7 @@ public class HexFeatureManager : MonoBehaviour
 {
     Transform container;
 
-    public Transform[] urbanPrefabs;
+    public HexFeatureCollection[] urbanCollections;
 
     public void Clear()
     {
@@ -26,7 +26,7 @@ public class HexFeatureManager : MonoBehaviour
         HexHash hash = HexMetrics.SampleHashGrid(position);
         
         //minimize population of features by certain percent
-        Transform prefab = PickPrefab(cell.UrbanLevel, hash.a);
+        Transform prefab = PickPrefab(cell.UrbanLevel, hash.a, hash.b);
 		if (!prefab) {
 			return;
 		}
@@ -37,18 +37,18 @@ public class HexFeatureManager : MonoBehaviour
         
 
         //add random rotation to object
-        instance.localRotation = Quaternion.Euler(0f, 360f * hash.b, 0f);
+        instance.localRotation = Quaternion.Euler(0f, 360f * hash.c, 0f);
         instance.SetParent(container, false);
     }
 
     //uses a level and hash value to select a prefab. If the level is larger than zero, we retrieve the thresholds using the level decreased by one. Then we loop through the thresholds until one exceeds 
     //the hash value. That means we found a prefab. If we didn't, we return null.
-    Transform PickPrefab (int level, float hash) {
+    Transform PickPrefab (int level, float hash, float choice) {
 		if (level > 0) {
 			float[] thresholds = HexMetrics.GetFeatureThresholds(level - 1);
 			for (int i = 0; i < thresholds.Length; i++) {
 				if (hash < thresholds[i]) {
-					return urbanPrefabs[i];
+					return urbanCollections[i].Pick(choice);
 				}
 			}
 		}
