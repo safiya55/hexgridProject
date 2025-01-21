23e189d846e4e64651b2365128abba4c57daf0e4
Adrian Ho <adrho9577@yahoo.ca>
2025-01-21 06:47:41 -0500

    3.2 Placing Walls On the Ground

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -112,7 +112,10 @@ public class HexFeatureManager : MonoBehaviour
 	{
 		if (nearCell.Walled != farCell.Walled)
 		{
-			AddWallSegment(near.v1, far.v1, near.v5, far.v5);
+			AddWallSegment(near.v1, far.v1, near.v2, far.v2);
+			AddWallSegment(near.v2, far.v2, near.v3, far.v3);
+			AddWallSegment(near.v3, far.v3, near.v4, far.v4);
+			AddWallSegment(near.v4, far.v4, near.v5, far.v5);
 		}
 	}
 
