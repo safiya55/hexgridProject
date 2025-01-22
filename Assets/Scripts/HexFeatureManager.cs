0d0a958c644a9060b76e83ccf97922a6a93fcbac
Adrian Ho <adrho9577@yahoo.ca>
2025-01-21 21:11:38 -0500

    done part 10

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -195,13 +195,25 @@ public class HexFeatureManager : MonoBehaviour
 			{
 				AddWallSegment(pivot, left, pivot, right);
 			}
+			else if (leftCell.Elevation < rightCell.Elevation)
+			{
+				AddWallWedge(pivot, left, right);
+			}
 			else
 			{
 				AddWallCap(pivot, left);
 			}
 		}
-		else if (hasRighWall) {
-			AddWallCap(right, pivot);
+		else if (hasRighWall)
+		{
+			if (rightCell.Elevation < leftCell.Elevation)
+			{
+				AddWallWedge(right, pivot, left);
+			}
+			else
+			{
+				AddWallCap(right, pivot);
+			}
 		}
 	}
 
@@ -261,5 +273,27 @@ public class HexFeatureManager : MonoBehaviour
 		v2 = v4 = center + thickness;
 		v3.y = v4.y = center.y + HexMetrics.wallHeight;
 		walls.AddQuadUnperturbed(v1, v2, v3, v4);
+	}
+
+	void AddWallWedge(Vector3 near, Vector3 far, Vector3 point)
+	{
+		near = HexMetrics.Perturb(near);
+		far = HexMetrics.Perturb(far);
+		point = HexMetrics.Perturb(point);
+
+		Vector3 center = HexMetrics.WallLerp(near, far);
+		Vector3 thickness = HexMetrics.WallThicknessOffset(near, far);
+
+		Vector3 v1, v2, v3, v4;
+		Vector3 pointTop = point;
+		point.y = center.y;
+
+		v1 = v3 = center - thickness;
+		v2 = v4 = center + thickness;
+		v3.y = v4.y = pointTop.y = center.y + HexMetrics.wallHeight;
+
+		walls.AddQuadUnperturbed(v1, point, v3, pointTop);
+		walls.AddQuadUnperturbed(point, v2, pointTop, v4);
+		walls.AddTriangleUnperturbed(pointTop, v3, v4);
 	}
 }
