eef2ee12c77a13aa3b4730b121f7d18bc3411745
Adrian Ho <adrho9577@yahoo.ca>
2025-01-27 15:08:35 -0500

    1.7 Locking The Camera

--- Assets/Scripts/NewMapMenu.cs
+++ Assets/Scripts/NewMapMenu.cs
@@ -1,14 +1,38 @@
 using UnityEngine;
 
-public class NewMapMenu : MonoBehaviour {
+public class NewMapMenu : MonoBehaviour
+{
 
 	public HexGrid hexGrid;
 
-	public void Open () {
+	public void Open()
+	{
 		gameObject.SetActive(true);
 	}
 
-	public void Close () {
+	public void Close()
+	{
 		gameObject.SetActive(false);
+	}
+
+	void CreateMap(int x, int z)
+	{
+		hexGrid.CreateMap(x, z);
+		Close();
+	}
+
+	public void CreateSmallMap()
+	{
+		CreateMap(20, 15);
+	}
+
+	public void CreateMediumMap()
+	{
+		CreateMap(40, 30);
+	}
+
+	public void CreateLargeMap()
+	{
+		CreateMap(80, 60);
 	}
 }
