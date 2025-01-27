0bc702ec48401926a0684d1464356982ce91a098
Adrian Ho <adrho9577@yahoo.ca>
2025-01-27 13:36:41 -0500

    1.6 Creating New Maps

--- Assets/Scripts/NewMapMenu.cs
+++ Assets/Scripts/NewMapMenu.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class NewMapMenu : MonoBehaviour {
 
 	public HexGrid hexGrid;
+
+	public void Open () {
+		gameObject.SetActive(true);
+	}
+
+	public void Close () {
+		gameObject.SetActive(false);
+	}
 }
