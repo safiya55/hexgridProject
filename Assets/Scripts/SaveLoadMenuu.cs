d66d73899c743f9f70f97343bfb34ebecbcafaf6
safiya55 <safiyaedwards@trentu.ca>
2025-01-28 22:59:26 -0500

    3.2 Changing Appearance

--- Assets/Scripts/SaveLoadMenuu.cs
+++ Assets/Scripts/SaveLoadMenuu.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SaveLoadMenu : MonoBehaviour {
 
-    public HexGrid hexGrid; 
+    public HexGrid hexGrid;
+    public Text menuLable, actionButtonLabel;
 
     public void Open(){
         gameObject.SetActive(true);
@@ -18,6 +19,14 @@ public class SaveLoadMenu : MonoBehaviour {
 
     public void Open(bool saveMode){
         this.saveMode = saveMode;
+        if(saveMode){
+            menuLabel.text = "Save Map";
+            actionButtonLabel.text = "Save";
+        }
+        else{
+            menuLable.text = "Load Map";
+            actionButtonLabel.text ="Load";
+        }
         gameObject.SetActive(true);
         HexMapCamera.Locked = true ;
     }
