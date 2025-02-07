a563346b7cdda1ad4dd84c335f709802911f0f32
Adrian Ho <54951013+Adrian-HO@users.noreply.github.com>
2025-02-07 16:26:27 -0500

    2.1 Wizard (adding menu item)

---
+++ Assets/Scripts/Editor/TextureArrayWizard.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+using UnityEngine;
+
+public class TextureArrayWizard : ScriptableWizard
+{
+    [MenuItem("Assets/Create/Texture Array")]
+    static void CreateWizard () {
+		ScriptableWizard.DisplayWizard<TextureArrayWizard>(
+			"Create Texture Array", "Create"
+		);
+	}
+}
