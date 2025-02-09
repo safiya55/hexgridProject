08d5ec97edde7d740ee8aa21cb07d6de762367d0
Adrian Ho <adrho9577@yahoo.ca>
2025-02-08 22:21:30 -0500

    2.2 Creating Something

--- Assets/Scripts/Editor/TextureArrayWizard.cs
+++ Assets/Scripts/Editor/TextureArrayWizard.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class TextureArrayWizard : ScriptableWizard
 {
+	public Texture2D[] textures;
+
     [MenuItem("Assets/Create/Texture Array")]
     static void CreateWizard () {
 		ScriptableWizard.DisplayWizard<TextureArrayWizard>(
