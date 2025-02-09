ba0b7aed4c93b83640cf3eb192cb42727ee9ae9d
Adrian Ho <adrho9577@yahoo.ca>
2025-02-09 12:36:56 -0500

    2.3 Crating a Texture Array

--- Assets/Scripts/Editor/TextureArrayWizard.cs
+++ Assets/Scripts/Editor/TextureArrayWizard.cs
@@ -5,10 +5,36 @@ public class TextureArrayWizard : ScriptableWizard
 {
 	public Texture2D[] textures;
 
-    [MenuItem("Assets/Create/Texture Array")]
-    static void CreateWizard () {
+	[MenuItem("Assets/Create/Texture Array")]
+	static void CreateWizard()
+	{
 		ScriptableWizard.DisplayWizard<TextureArrayWizard>(
 			"Create Texture Array", "Create"
 		);
+	}
+
+	//method that gets invoked when the create button is pressed
+	void OnWizardCreate()
+	{
+		//where we create texture array if user has added any texture to the wizard
+		if (textures.Length == 0)
+		{
+			//abort if none is created
+			return;
+		}
+
+		//ask the user where to save the texture array asset. 
+		//parameters determine the panel name, default file name, the file extension, and description.
+		//Texture arrays use the generic asset file extension
+		string path = EditorUtility.SaveFilePanelInProject( //returns the file path that the user selected.
+			"Save Texture Array", "Texture Array", "asset", "Save Texture Array"
+		);
+
+		//f the user canceled the panel, then the path will be the empty string.
+		if (path.Length == 0)
+		{
+			//abort
+			return;
+		}
 	}
 }
