09c8a291239cdb9335f946c4e4c4d188167b2740
Adrian Ho <adrho9577@yahoo.ca>
2025-02-16 15:12:35 -0500

    4.2 Adding To The Queue

---
+++ Assets/Scripts/HexCellPriorityQueue.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+public class HexCellPriorityQueue
+{
+
+    List<HexCell> list = new List<HexCell>();
+
+    int count = 0;
+
+    public int Count
+    {
+        get
+        {
+            return count;
+        }
+    }
+
+    public void Enqueue(HexCell cell)
+    {
+        count += 1;
+    }
+
+    public HexCell Dequeue()
+    {
+        count -= 1;
+        return null;
+    }
+
+    public void Change(HexCell cell, int oldPriority)
+    {
+    }
+
+    public void Clear()
+    {
+        list.Clear();
+        count = 0;
+    }
+}
