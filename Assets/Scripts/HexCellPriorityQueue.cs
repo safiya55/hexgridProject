3fde3df59fe4339e0d07e33b5ed7833de0d81769
Adrian Ho <adrho9577@yahoo.ca>
2025-02-16 15:26:09 -0500

    4.6 Using the Queue

--- Assets/Scripts/HexCellPriorityQueue.cs
+++ Assets/Scripts/HexCellPriorityQueue.cs
@@ -50,6 +50,36 @@ public class HexCellPriorityQueue
 
     public void Change(HexCell cell, int oldPriority)
     {
+        //declaring the head of the old priority list to be the current cell
+        HexCell current = list[oldPriority];
+        //keep track of the next cell. We can directly grab the next cell
+        HexCell next = current.NextWithSamePriority;
+
+        //If the current cell is the changed cell, then it is the head cell 
+        // and we can cut it away, as if we dequeued it.
+        if (current == cell)
+        {
+            list[oldPriority] = next;
+        }
+
+        else //we have to follow the chain until we end up at the cell in front of the changed cell. 
+        // That one holds the reference to the cell that has been changed.
+        {
+            while (next != cell)
+            {
+                current = next;
+                next = current.NextWithSamePriority;
+            }
+            //can remove the changed cell from the linked list, by skipping it.
+            current.NextWithSamePriority = cell.NextWithSamePriority;
+
+            //cell removed will be added gain so it ends 
+            // up in the list for its new priority.
+            Enqueue(cell);
+
+            //decrement count
+            count -= 1;
+        }
     }
 
     public void Clear()
