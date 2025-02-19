cfe8bd8e4a914b26fd7e0a550edd731f22a8ab85
Adrian Ho <adrho9577@yahoo.ca>
2025-02-19 01:18:53 -0500

    3.4 Selecting a Unit

--- Assets/Scripts/HexGameUI.cs
+++ Assets/Scripts/HexGameUI.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class HexGameUII : MonoBehaviour
 {
+
+    //HexGameUI needs to know which cell is currently underneath the cursor.
+    HexCell currentCell;
     public HexGrid grid;
 
     //game UI should be enabled when we're not in edit mode. Also, this is the place to toggle the labels, 
@@ -10,5 +13,19 @@ public class HexGameUII : MonoBehaviour
     {
         enabled = !toggle;
         grid.ShowUI(!toggle);
+    }
+
+    //bool to updating the current cell, we might like to know whether it has changed.
+    bool UpdateCurrentCell()
+    {
+        //uses HexGrid.GetCell with the cursor ray, to update the field.
+        HexCell cell =
+            grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (cell != currentCell)
+        {
+            currentCell = cell;
+            return true;
+        }
+        return false;
     }
 }
