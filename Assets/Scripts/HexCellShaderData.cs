82d23059607001f47c05f3072eb851e1ae1f54dd
Adrian Ho <adrho9577@yahoo.ca>
2025-02-21 22:34:11 -0500

    1.3 updating Cell Data

--- Assets/Scripts/HexCellShaderData.cs
+++ Assets/Scripts/HexCellShaderData.cs
@@ -9,6 +9,8 @@ public class HexCellShaderData : MonoBehaviour
 
     List<HexCell> transitioningCells = new List<HexCell>();
 
+    const float transitionSpeed = 255f;
+
     //toggle whether we want immediate transitions. 
     public bool ImmediateMode { get; set; }
 
@@ -59,13 +61,37 @@ public class HexCellShaderData : MonoBehaviour
 
     void LateUpdate()
     {
+        //get delta
+        int delta = (int)(Time.deltaTime * transitionSpeed);
+        //It is also theoretically possible to get very high frame rates. 
+        // Together with a low transition speed, this could result in a delta of zero. 
+        //To guarantee progress, force the delta to have a minimum of 1.
+        if (delta == 0)
+        {
+            delta = 1;
+        }
+
+        //loop through all transitioning cells and update their data.
+        for (int i = 0; i < transitioningCells.Count; i++)
+        {
+            if (!UpdateCellData(transitioningCells[i], delta))
+            {
+                //move the last cell to the current index and then remove the last one.
+                transitioningCells[i--] =
+                    transitioningCells[transitioningCells.Count - 1];
+                transitioningCells.RemoveAt(transitioningCells.Count - 1);
+            }
+        }
+
         cellTexture.SetPixels32(cellTextureData);
         cellTexture.Apply();
-        enabled = false;
+        enabled = transitioningCells.Count > 0;
     }
 
     public void RefreshVisibility(HexCell cell)
     {
+        //determine the delta to apply to the values, by multiplying the time delta with the speed. This has to be an integer, because we don't know how 
+        // large it could get. A freak frame-rate dip could make the delta larger than 255.
         int index = cell.Index;
         if (ImmediateMode)
         {
@@ -75,7 +101,20 @@ public class HexCellShaderData : MonoBehaviour
         else
         {
             transitioningCells.Add(cell);
-        } 
-        enabled = true;
+        }
+        enabled = transitioningCells.Count > 0;
+    }
+
+    bool UpdateCellData(HexCell cell, int delta)
+    {
+        //going to need the cell's index and data to do its work
+        int index = cell.Index;
+        Color32 data = cellTextureData[index];
+        bool stillUpdating = false;
+
+        //has to determine whether this cell still requires further updating.
+        //adjusted data has to be applied and the still-updating status returned.
+        cellTextureData[index] = data;
+        return stillUpdating;
     }
 }
