427546b3e26ba492bec1fad73cdabd8483bd2745
Adrian Ho <adrho9577@yahoo.ca>
2025-02-23 21:18:12 -0500

    2.2 Creating a Single Chunk

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -6,16 +6,33 @@ public class HexMapGenerator : MonoBehaviour
 
     public HexGrid grid;
 
+    int cellCount;
+
     public void GenerateMap(int x, int z)
     {
+        //keep track of the amount of cells in HexMapGenerator 
+        cellCount = x * z;
+
         //new map new map created
         grid.CreateMap(x, z);
 
         //set the terrain of the middle 
         // cell column to grass, using offset coordinates.
-        for (int i = 0; i < z; i++)
+        RaiseTerrain(7);
+    }
+
+    void RaiseTerrain(int chunkSize)
+    {
+        for (int i = 0; i < chunkSize; i++)
         {
-            grid.GetCell(x / 2, i).TerrainTypeIndex = 1;
+            GetRandomCell().TerrainTypeIndex = 1;
         }
+    }
+
+    //determines a random cell index and retrieves the 
+    // corresponding cell from the grid.
+    HexCell GetRandomCell()
+    {
+        return grid.GetCell(Random.Range(0, cellCount));
     }
 }
