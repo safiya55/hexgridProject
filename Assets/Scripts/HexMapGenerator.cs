087e679283fabd3e5f62a237109f0b8f00d08ff7
Adrian Ho <adrho9577@yahoo.ca>
2025-02-23 20:48:44 -0500

    1.3 Adjusting the New Map Menu

---
+++ Assets/Scripts/HexMapGenerator.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMapGenerator : MonoBehaviour {
+
+	public HexGrid grid;
+
+	public void GenerateMap (int x, int z) {
+		grid.CreateMap(x, z);
+	}
+}
