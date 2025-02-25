577bfa9edcf43f3817cabf96bdc9682f426b1edf
Adrian Ho <adrho9577@yahoo.ca>
2025-02-25 11:09:53 -0500

    Keeping The Cells Together

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -8,6 +8,10 @@ public class HexMapGenerator : MonoBehaviour
 
     int cellCount;
 
+    HexCellPriorityQueue searchFrontier;
+
+    int searchFrontierPhase;
+
     public void GenerateMap(int x, int z)
     {
         //keep track of the amount of cells in HexMapGenerator 
@@ -16,17 +20,62 @@ public class HexMapGenerator : MonoBehaviour
         //new map new map created
         grid.CreateMap(x, z);
 
+        //Make sure that the priority queue exists before we will need it.
+        if (searchFrontier == null)
+        {
+            searchFrontier = new HexCellPriorityQueue();
+        }
+
         //set the terrain of the middle 
         // cell column to grass, using offset coordinates.
         RaiseTerrain(7);
+
+        //After a new map has been created
+
+        for (int i = 0; i < cellCount; i++)
+        {
+            //search frontier of all cells is zero.
+            grid.GetCell(i).SearchPhase = 0;
+        }
     }
 
     void RaiseTerrain(int chunkSize)
     {
-        for (int i = 0; i < chunkSize; i++)
-        {
-            GetRandomCell().TerrainTypeIndex = 1;
+        // search for appropriate cells 
+        //increasing the search frontier phase by 1
+        searchFrontierPhase += 1;
+        //Then initialize the frontier with the first cell by random
+        HexCell firstCell = GetRandomCell();
+        firstCell.SearchPhase = searchFrontierPhase;
+        //set its distance and heuristic to zero besides setting its search phase
+        firstCell.Distance = 0;
+        firstCell.SearchHeuristic = 0;
+        searchFrontier.Enqueue(firstCell);
+
+        int size = 0;
+        while (size < chunkSize && searchFrontier.Count > 0)
+        { // Each iteration, dequeue the next cell, set its terrain type, increase the size, 
+        // then go through that cell's neighbors
+
+        //All neighbors are simply added to the frontier
+            HexCell current = searchFrontier.Dequeue();
+            current.TerrainTypeIndex = 1;
+            size += 1;
+
+            for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
+            {
+                HexCell neighbor = current.GetNeighbor(d);
+                if (neighbor && neighbor.SearchPhase < searchFrontierPhase)
+                {
+                    neighbor.SearchPhase = searchFrontierPhase;
+                    neighbor.Distance = 0;
+                    neighbor.SearchHeuristic = 0;
+                    searchFrontier.Enqueue(neighbor);
+                }
+            }
         }
+        //Once done, clear the frontier.
+        searchFrontier.Clear();
     }
 
     //determines a random cell index and retrieves the 
